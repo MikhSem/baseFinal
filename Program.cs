68ec1b089a9f81a4a7b1bc1af16c2a1d180b9d61
Mikhail <cdr15@rambler.ru>
2023-02-19 11:21:00 +0300

    Добавил описание кода

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] sourceArray = new string[5] { "4785", "11", "привет", "мама", "сон" };
+string[] targetArray = new string[sourceArray.Length];
+
+void ModifiedArray(string[] sourceArray, string[] targetArray)
+{
+    int count = 0;
+    for (int i = 0; i < sourceArray.Length; i++)
+    {
+        if (sourceArray[i].Length <= 3)
+        {
+            targetArray[count] = sourceArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ModifiedArray(sourceArray, targetArray);
+PrintArray(sourceArray);
+PrintArray(targetArray);
